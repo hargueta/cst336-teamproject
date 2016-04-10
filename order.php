06cd625b85df39c87fd44f03d5c3aaf837d83b6c
miriama230 <miflores@csumb.edu>
2016-04-10 05:31:50 +0000

    basic structore of the order page

---
+++ order.php
@@ -0,0 +1,137 @@
+<?php
+    include('../../includes/database.php');
+    $dbConnection = getDatabaseConnection('simple_pizza');
+    
+    function getPizzas()
+    {
+        global $dbConnection;
+    
+        $sql = "SELECT name  FROM pizza";
+        $statement = $dbConnection->prepare($sql);
+        $statement->execute();
+        $records = $statement->fetchAll(PDO::FETCH_ASSOC);
+    
+        return $records;
+    }
+    function getDrinks()
+    {
+        global $dbConnection;
+    
+        $sql = "SELECT name  FROM drinks";
+        $statement = $dbConnection->prepare($sql);
+        $statement->execute();
+        $records = $statement->fetchAll(PDO::FETCH_ASSOC);
+    
+        return $records;
+    }
+    function getAppetizer()
+    {
+        global $dbConnection;
+    
+        $sql = "SELECT name  FROM appetizers";
+        $statement = $dbConnection->prepare($sql);
+        $statement->execute();
+        $records = $statement->fetchAll(PDO::FETCH_ASSOC);
+    
+        return $records;
+    }
+    function getDesserts()
+    {
+         global $dbConnection;
+    
+        $sql = "SELECT name  FROM desserts";
+        $statement = $dbConnection->prepare($sql);
+        $statement->execute();
+        $records = $statement->fetchAll(PDO::FETCH_ASSOC);
+    
+        return $records;
+    }
+    
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> </title>
+    </head>
+    <body>
+            Filter by: 
+            <input type = "radio" name = "sortBy" value = "Pizza"> Pizza
+            <input type = "radio" name = "sortBy" value = "Drinks"> Drinks
+            <input type = "radio" name = "sortBy" value = "Appetizer"> Appetizers
+            <h1>Specialty Pizzas</h1>
+            <?php
+                 $pizzas = getPizzas();
+                 echo "<table>";
+                
+                $count  =0;
+                 foreach($pizzas as $item)
+                {
+                   
+                    if($count == 0 || $count == 5)
+                        echo "<tr>";
+                    
+                    echo"<td>";
+                    echo "<img src = 'img/".($count+1).".jpg' width = 100 alt = 'picture of a pizza'>";
+                    echo"</br>";
+                    echo "<input type ='radio' value = ".$item['name'].">".$item['name'];
+                    echo"</td>";
+                    
+                    if($count == 1 && $count == 5)
+                        echo "</tr>";
+                    $count++;
+                }
+                echo"</table>";
+            ?>
+            Drinks
+            <select>
+                <?php
+                    $drinks = getDrinks();
+                    foreach($drinks as $drink)
+                    {
+                        echo "<option value = ".$drink['name']." >".$drink['name']."</option>";
+                    }
+                ?>
+            </select>
+            <h1>Appetizers</h1>
+            
+             <?php
+                 $appetizer = getAppetizer();
+                 echo "<table>";
+                
+                $count  =0;
+                 foreach($appetizer as $item)
+                {
+                      
+                    echo"<td>";
+                    echo "<img src = 'img/1".($count+1).".jpg' width = 100 alt = 'picture of a appetizers'>";
+                    echo"</br>";
+                    echo "<input type ='checkbox' value = ".$item['name'].">".$item['name'];
+                    echo"</td>";
+                    
+                    $count++;
+                }
+                echo"</table>";
+            ?>
+            <h1>Desserts</h1>
+             <?php
+                 $desserts = getDesserts();
+                 echo "<table>";
+                
+                $count  =16;
+                 foreach($desserts as $item)
+                {
+                      
+                    echo"<td>";
+                    echo "<img src = 'img/".($count).".jpg' width = 100 alt = 'picture of a appetizers'>";
+                    echo"</br>";
+                    echo "<input type ='checkbox' value = ".$item['name'].">".$item['name'];
+                    echo"</td>";
+                    
+                    $count++;
+                }
+                echo"</table>";
+            ?>   
+           <input type = "submit" value = "submit">    
+           
+    </body>
+</html>
