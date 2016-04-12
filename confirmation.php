f6a29cf609857567adc5ddff9ad657d170a26892
Mariela Ceja <maceja@csumb.edu>
2016-04-12 22:21:13 +0000

    Updated  confirmation.php

--- confirmation.php
+++ confirmation.php
@@ -8,7 +8,7 @@ function getUserId(){
     global $connection;
     global $userId;
     
-    $sql = "SELECT usernameId FROM users WHERE username='".$_SESSION['username']."'";
+    $sql = "SELECT userId FROM users WHERE username='".$_SESSION['username']."'";
     $statement = $connection->prepare($sql);
     $statement->execute();
     $userId = $statement->fetch();
@@ -66,7 +66,7 @@ function getTotal()
     global $userId;
     global $connection;
         
-    $sql = "SELECT price FROM order WHERE userId='" . $userId. "'";
+    $sql = "SELECT price FROM `order` WHERE userId='".$userId."'";
     $statement = $connection->prepare($sql);
     $statement->execute();
     $records = $statement->fetchAll(PDO::FETCH_ASSOC);
@@ -77,7 +77,7 @@ function getTax(){
     global $userId;
     global $connection;
         
-    $sql = "SELECT price FROM order WHERE userId='" . $userId. "'";
+    $sql = "SELECT price FROM `order` WHERE userId='".$userId."'";
     $statement = $connection->prepare($sql);
     $statement->execute();
     $records = $statement->fetch();
